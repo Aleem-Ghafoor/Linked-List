6dabca0ba572e9e20c5644fc3bf1aaef50633505
Aleem-Ghafoor <aleemghfr@gmail.com>
2025-06-30 20:12:20 +0500

    Update from struct to classes

--- linkedlist_transversal.cpp
+++ linkedlist_transversal.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 
-struct Node{
+class Node{
+  private:
     int n;
     Node* next;
-};
+  public:
 
 void insertdata(Node* &head, int n){
 
@@ -28,24 +29,32 @@ void insertdata(Node* &head, int n){
 }
 
 // search perticular value
-/*
-void search(Node* &head, int n){
+
+void search(Node* &head){
+
+    int n, a = 0;
+
+    cout << "\nEnter the number you want to search: ";
+    cin >> n;
 
     Node* temp = head;
-
+  
     while(temp != NULL){
+        a++;
+
         if(temp->n == n){
-            cout << "The number is found: " << temp->n;
+            cout << "The number " << temp->n << " at " << a << " place. " << endl;
             return;
         }
+        
         temp = temp->next;
     }
         cout << "Not found!";
-}*/
+}
 
 //find length
 
-/*void length(Node* &head){
+void length(Node* &head){
       int a = 0;
       Node* temp = head;
 
@@ -53,31 +62,32 @@ void search(Node* &head, int n){
           a++;
           temp = temp->next;
       }
-      cout << "\n\nThe length is: " << a;
-}*/
+      cout << "\nThe length is: " << a << endl;
+}
 
 //update head
 
-/* void change_head(Node* &head, Node* &newhead){
+ void change_head(Node* &head, Node* &newhead){
     int n;
 
     cout << "\nEnter the value: ";
     cin >> newhead->n;
 
     newhead->next = head->next;
+    
+    cout << "\nFirst place value changed from " << head->n << " to " << newhead->n << endl;
 
     while(head != NULL){
         Node* temp = head;
         head = head->next;
-        delete temp;
-
+        delete temp; 
     }
-} */
+} 
 
 // delete list
 
 void del(Node* &head){
-    int n;
+    int n, a = 0;
 
         cout << "\nEnter the number to remove list: ";
         cin >> n;
@@ -86,7 +96,7 @@ void del(Node* &head){
         Node* perv = NULL;
 
         while(temp != NULL){
-
+            a++;
             if(temp->n == n){
                 
                 if(temp == head){
@@ -98,7 +108,7 @@ void del(Node* &head){
                     delete temp;
                 }
 
-                cout << "Delete successful!";
+                cout << a << " place Delete successfully!";
                 return;
             }
             perv = temp;
@@ -107,49 +117,87 @@ void del(Node* &head){
         }
 }
 
+// display
+void display(Node* &head){
+       Node* temp = head;
+       while(temp != NULL){
+       cout << "\nThe value is: " << temp->n;
+       temp = temp->next;
+       } 
+}
+
+//list
+int list(){
+    int choice;
+
+    while(true){
+       cout << "\n----------Chose from the following----------\n";
+       cout << "\n1-Find length";
+       cout << "\n2-Find value";
+       cout << "\n3-Change head";
+       cout << "\n4-Delete List";
+       cout << "\n5-View List";
+       cout << "\n6-Exit";
+       cout << "\n\nEnter the choice: ";
+       cin >> choice;
+       
+       if(choice > 0 && choice < 7) break;
+       else cout << "\nInvalid number!";
+    }
+
+    return choice;
+}
+
+};
+
 int main(){
-    int n;
+    int n, a = 0;
     Node* head = NULL;
     Node* newhead = new Node();
     
-    for(int i = 0; i < 3; i++){
-         cout << "\nEnter the n: ";
+    cout << "\nPress -1 to stop!";
+    while(true){
+         cout << "\n" << ++a << "-Enter the value: ";
          cin >> n;
+         if(n == -1) break;
 
-         insertdata(head, n);
+         head->insertdata(head, n);
     }
-
-    del(head);
-
-//Update head 
-
-   // change_head(head, newhead);
-
-// length
-
-    //length(head);
-
-// search single linked list
-
-    /*cout << "Enter the number you want to search: ";
-    cin >> n;
-    search(head, n);*/
-
-// print new head
-
-    /* Node* temp = newhead;
-       while(temp != NULL){
-       cout << "\nThe n is: " << temp->n;
-       temp = temp->next;
-       }*/
-
-//print data
-
-     Node* temp = head;
-       while(temp != NULL){
-       cout << "\nThe n is: " << temp->n;
-       temp = temp->next;
-       } 
     
+        a = 0;
+        int choice = 0;
+        
+do{
+    choice = head->list();
+
+    switch(choice){
+        case 1:
+            head->length(head);
+        break;
+        case 2:
+            head->search(head);
+        break;
+        case 3:
+            head->change_head(head, newhead);
+            a++;
+        break;
+        case 4:
+            if(a > 0)
+               head->del(newhead);
+            else
+               head->del(head);
+        break;
+        case 5:
+            if(a > 0)
+               head->display(newhead);
+            else
+               head->display(head);
+        break;
+        case 6:
+        break;
+        default:
+            cout << "Invalid Number!";
+    }
+}while(choice != 6);
     return 0;
 }
